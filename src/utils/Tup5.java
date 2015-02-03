f3b0479e650eff157870650448ee59bf7b58bd7c
Adrian Benton <adrian.benton@gmail.com>
2015-02-03 03:09:03 -0500

    Added untracked files

---
+++ src/utils/Tup5.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class Tup5<V1, V2, V3, V4, V5> {
+	
+	private V1 v1;
+	private V2 v2;
+	private V3 v3;
+	private V4 v4;
+	private V5 v5;
+	
+	public Tup5(V1 v10, V2 v20, V3 v30, V4 v40, V5 v50) {
+		v1 = v10;
+		v2 = v20;
+		v3 = v30;
+		v4 = v40;
+		v5 = v50;
+	}
+	
+	public V1 _1() { return v1; };
+	public V2 _2() { return v2; };
+	public V3 _3() { return v3; };
+	public V4 _4() { return v4; };
+	public V5 _5() { return v5; };
+	
+}
