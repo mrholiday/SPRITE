2d0bacc2a7bca47470a8eb44c57abfad979763b4
Adrian Benton <adrian.benton@gmail.com>
2015-04-02 14:32:14 -0400

    Added untracked

---
+++ src/models/factored/impl/SpriteSingleViewLinkedSupertopics.java
@@ -0,0 +1,70 @@
+package models.factored.impl;
+
+import models.factored.Factor;
+import models.factored.LinkedFactor;
+import models.factored.SpriteFactoredTopicModel;
+import prior.SpritePhiPrior;
+import prior.SpriteThetaPrior;
+import utils.ArgParse;
+import utils.Log;
+import utils.ArgParse.Arguments;
+import utils.Tup3;
+
+/**
+ * Topic model with single latent supertopic factor for one view.
+ * Delta/Beta ensure that each supertopic has a single child, and
+ * Alpha is constrained to be positive.  This should be equivalent
+ * to LDA.
+ * 
+ * @author adrianb
+ *
+ */
+public class SpriteSingleViewLinkedSupertopics extends SpriteFactoredTopicModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8356557959212672700L;
+	
+	public SpriteSingleViewLinkedSupertopics(SpriteThetaPrior[] thetaPriors0,
+			SpritePhiPrior[] phiPriors0, Factor[] factors0, int numThreads0,
+			double stepSize0) {
+		super(thetaPriors0, phiPriors0, factors0, numThreads0, stepSize0);
+	}
+	
+	private static Tup3<Factor[], SpriteThetaPrior[], SpritePhiPrior[]> buildPriors(int Z, int C,
+				double sigmaDeltaBias, double initDeltaBias, double sigmaOmegaBias, double initOmegaBias,
+				double sigmaBeta, double sigmaOmega, double sigmaAlpha, double sigmaDelta) {
+		Factor[] factors = new Factor[] {new LinkedFactor(C, new int[] {0}, new int[] {Z}, 1.0, true,
+										 sigmaBeta, sigmaOmega, sigmaAlpha, sigmaDelta, true,
+										 false, false, "supertopic", false, true, true, 1.0)};
+		
+		SpriteThetaPrior thetaPrior1 = new SpriteThetaPrior(factors, Z, new int[] {0, 1}, initDeltaBias, sigmaDeltaBias, true);
+		SpritePhiPrior phiPrior1 = new SpritePhiPrior(factors, Z, new int[] {0}, initOmegaBias, sigmaOmegaBias, true);
+		SpriteThetaPrior[] tpriors = {thetaPrior1};
+		SpritePhiPrior[]   ppriors = {phiPrior1};
+		
+		return new Tup3<Factor[], SpriteThetaPrior[], SpritePhiPrior[]>(factors, tpriors, ppriors);
+	}
+	
+	public static void main(String[] args) {
+		SpriteFactoredTopicModel topicModel = null;
+		
+		Arguments p = ArgParse.parseArgs(args);
+		
+		if (p != null) {
+			Tup3<Factor[], SpriteThetaPrior[], SpritePhiPrior[]> graph = buildPriors(p.z, p.C, p.sigmaDeltaBias, p.deltaBias, p.sigmaOmegaBias,
+																					 p.omegaBias, p.sigmaBeta, p.sigmaOmega, p.sigmaAlpha, p.sigmaDelta);
+			topicModel = new SpriteSingleViewLinkedSupertopics(graph._2(), graph._3(), graph._1(), p.numThreads, p.step);
+			topicModel.outputDir = p.outDir;
+			topicModel.TIME_ITERATIONS = true;
+			
+			try {
+				topicModel.train(p.iters, p.samples, p.filename);
+			} catch (Exception e) {
+				Log.error("train", "Error in training...", e);
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
